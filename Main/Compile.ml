a85486746a382662c816ea73717dd449cd25280a
mtache <mtache@cisco.com>
2017-02-26 15:39:03 +0100

    Changed skeleton

--- Main/Compile.ml
+++ Main/Compile.ml
@@ -4,7 +4,9 @@ let build_method_table c_ast c_name =
     let rec iter c_name methods =
         match methods with
             | [] -> Env.initial()
-            | m::others -> (Env.define (iter c_name others) (c_name^"_"^m.mname) m)
+            | m::others -> let env = (iter c_name others) and key = c_name^"_"^m.mname
+                           in if Env.mem env key then Error.environment_duplicate key
+                           else Env.define env key m (* TO BE TESTED *)
     in iter c_name c_ast.cmethods
 
 let build_object_descriptor c_ast c_name =
@@ -31,7 +33,26 @@ let parser ast f =
      | h::t -> (type_parse h)::(type_list_parse t)
   in type_list_parse ast.type_list
 
+let execute lexbuf verbose = 
   let init ast =
     let method_table = concat_method_tables (parser ast build_method_table)
     and object_descriptor_table = build_object_descriptor_table (parser ast build_object_descriptor)
-    in (method_table, object_descriptor_table);
+    in (method_table, object_descriptor_table)
+  in
+  try 
+   (* MAIN ALGORITHM *)
+    let ast = Parser.compilationUnit Lexer.token lexbuf in
+    let (method_table, object_descriptor_table) = init ast in
+    print_endline "successfull parsing";
+    if verbose then AST.print_program ast;
+    if Typing.check_class ast method_table object_descriptor_table
+    then print_endline "successfull typing check (unimplemented)"
+    else print_endline "unsuccessfull typing check (unimplemented)";
+    (* END - MAIN ALGORITHM *)
+  with 
+    | Parser.Error ->
+      print_string "Syntax error: ";
+      Location.print (Location.curr lexbuf)
+    | Error.Error(e,l) ->
+      Error.report_error e;
+      Location.print l

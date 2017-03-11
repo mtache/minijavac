b0b0085ecf6668e316ab800c39f485711d953370
theophile debauche <tetatoto.142857@gmail.com>
2017-03-11 18:45:47 +0100

    expression execution - beginning

--- Execution/Execution.ml
+++ Execution/Execution.ml
@@ -21,7 +21,7 @@ let print_memory env =
   let print_var env =
     print_endline " Variable :";
     Env.iter (fun (key,value) ->
-    print_endline ("    "^key^" of value "^(string_of_int value))) env
+    print_endline ("    "^key^" of value "^(value))) env
   in Env.iter (fun (key,value) -> 
   print_endline ("Type : "^key);
   print_var value) env
@@ -55,18 +55,66 @@ let modify_variable_mem variable_type new_value name mem =
 (* Some functions to deal with the dark type from the AST *)
 
 
-
+(* TODO : deal with each type diffrently *)
 let get_string_type_from_typet obscur_type =
 	Type.stringOf obscur_type
+
+(* Function to execute a simple operation *)
+
+let execute_op e1 inf_op e2 exp_type=
+	match inf_op with
+		| Op_add -> "0"(* 
+				let res = match exp_type with
+					| Primitive prim -> 
+						match prim with
+							| Int -> 
+
+
+				in
+ 				let a = get_value_of_exp_desc e1.edesc in
+				let b = get_value_of_exp_desc e2.edesc in
+				let  *)
+
+
+		| Op_sub -> "0"  
+		| Op_mul -> "0"  
+		| Op_div -> "0"  
+		| Op_mod -> "0"
+		| Op_cor -> "0"  
+		| Op_cand -> "0" 
+		| Op_or -> "0"   
+		| Op_and -> "0"  
+		| Op_xor -> "0"  
+		| Op_eq -> "0"   
+		| Op_ne -> "0"   
+		| Op_gt -> "0"   
+		| Op_lt -> "0"   
+		| Op_ge -> "0"   
+		| Op_le -> "0"   
+		| Op_shl -> "0"  
+		| Op_shr -> "0"  
+		| Op_shrr -> "0" 
+
+(* let string_to_real_value given_value val_type =
+	match val_type with
+		| Void -> given_value
+  		| Array of t * int
+  		| Primitive of primitive
+  		| Ref of ref_type *)
+		  
 	
 
-
-let get_value_of_exp exp =
+(* TODO : add all the type of exp *)
+let get_value_of_exp_desc exp exp_type=
 	match exp with
+		| Op(e1, inf_op, e2) -> 
+				(execute_op e1 inf_op e2 exp_type)
 		| Val x -> match x with
-			| Int n -> int_of_string n
-			| _ -> print_endline "not implemented"; 0
-		| _ -> print_endline "not implemented"; 0
+			| Int n -> n
+			| _ -> print_endline "not implemented"; "0"
+		| _ -> print_endline "not implemented"; "0"
+
+
 
 (* The functions to execute a variable declaration *)
 
@@ -75,7 +123,11 @@ let execute_vardecl_aux one_vd mem= match one_vd with
 	| (type_ast, name, Some(exp_value)) -> 
 		let variable_type = get_string_type_from_typet type_ast in
 		let expdesc_of_exp = exp_value.edesc in
-		let var_value = get_value_of_exp expdesc_of_exp in
+		let given_type = exp_value.etype in
+		let var_value = get_value_of_exp_desc expdesc_of_exp given_type in
+		(match given_type with
+				| None -> print_endline "lollololol"
+				| Some(type_found) -> print_endline ("LOOK HERE"^(Type.stringOf type_found)));
 		add_new_variable_to_mem variable_type var_value name mem
 	| _ -> print_endline "not implemented"; mem
 
@@ -85,10 +137,7 @@ let rec execute_vardecl vd_list mem = match vd_list with
 	| [] -> print_endline "vardecl finished or empty"; mem
 	| h::q -> 
 		let new_mem = execute_vardecl_aux h mem in
-				(
-				print_endline "AFTER the first statement";
-				print_memory new_mem;
-				execute_vardecl q new_mem )
+				(execute_vardecl q new_mem )
 
 
 (* the MAIN algorithm : *)
--- Tests/TestsUnitairesExec.java
+++ Tests/TestsUnitairesExec.java
@@ -3,7 +3,7 @@ public class A {
     public static void main() { 
         int x = 1;
         int d = 4;
-        int f, g;
+        int z = 1 + 2;
     }
 
     public void m2() {

5e646bef26b89589619bbd91d33ae631beee36c5
Bartosz Gorlewicz <bartosz.gorlewicz@tlecom-bretagne.fr>
2017-03-12 17:54:57 +0100

    Basic int operations working

--- Execution/Execution.ml
+++ Execution/Execution.ml
@@ -22,7 +22,7 @@ let print_memory env =
     print_endline " Variable :";
     Env.iter (fun (key,value) ->
     print_endline ("    "^key^" of value "^(value))) env
-  in Env.iter (fun (key,value) -> 
+  in Env.iter (fun (key,value) ->
   print_endline ("Type : "^key);
   print_var value) env
 
@@ -42,7 +42,7 @@ let add_new_variable_to_mem variable_type value name mem =
 		(print_endline "test1";
 		print_memory mem;
 		(Env.replace mem variable_type (Env.define (get_sub_env_by_type variable_type mem) name value));)
-	else 
+	else
 		let new_mem = add_new_type_to_mem variable_type mem in
 		(print_endline "test2";
 		print_memory new_mem;
@@ -59,72 +59,71 @@ let modify_variable_mem variable_type new_value name mem =
 let get_string_type_from_typet obscur_type =
 	Type.stringOf obscur_type
 
-(* Function to execute a simple operation *)
-
-let execute_op e1 inf_op e2 exp_type=
-	match inf_op with
-		| Op_add -> "0"(* 
-				let res = match exp_type with
-					| Primitive prim -> 
-						match prim with
-							| Int -> 
 
 
-				in
- 				let a = get_value_of_exp_desc e1.edesc in
-				let b = get_value_of_exp_desc e2.edesc in
-				let  *)
+		(* Function to execute a simple operation *)
+let int_operation_exec e1 op e2 =
+		let val1 = int_of_string e1 in
+		let val2 = int_of_string e2 in
+		match op with
+			| Op_add -> print_endline ("Added"^string_of_int (val1+ val2));
+									string_of_int (val1+ val2)
+
+			| Op_sub -> print_endline ("sub"^string_of_int (val1- val2));
+									string_of_int (val1- val2)
+
+			| Op_mul -> print_endline ("mul"^string_of_int (val1* val2));
+									string_of_int (val1 * val2)
+
+			| Op_div -> print_endline ("div"^string_of_int (val1/val2));
+									string_of_int (val1 / val2)
+
+			(*)| Op_mod -> None
+			| Op_cor -> None
+			| Op_cand -> None
+			| Op_or -> None
+			| Op_and -> None
+			| Op_xor -> None
+			| Op_eq -> None
+			| Op_ne -> None
+			| Op_gt -> None
+			| Op_lt -> None
+			| Op_ge -> None
+			| Op_le -> None
+			| Op_shl -> None
+			| Op_shr -> None
+			| Op_shrr -> None *)
 
 
-		| Op_sub -> "0"  
-		| Op_mul -> "0"  
-		| Op_div -> "0"  
-		| Op_mod -> "0"
-		| Op_cor -> "0"  
-		| Op_cand -> "0" 
-		| Op_or -> "0"   
-		| Op_and -> "0"  
-		| Op_xor -> "0"  
-		| Op_eq -> "0"   
-		| Op_ne -> "0"   
-		| Op_gt -> "0"   
-		| Op_lt -> "0"   
-		| Op_ge -> "0"   
-		| Op_le -> "0"   
-		| Op_shl -> "0"  
-		| Op_shr -> "0"  
-		| Op_shrr -> "0" 
 
-(* let string_to_real_value given_value val_type =
-	match val_type with
-		| Void -> given_value
-  		| Array of t * int
-  		| Primitive of primitive
-  		| Ref of ref_type *)
-		  
-	
+			let execute_op e1 inf_op e2 exp_type=
+			match exp_type with
+				| None -> print_endline "Type not found"; "Not found"
+				| Some(e) -> 			match e with
+														| Primitive prim -> match prim with
+																	| Int ->  int_operation_exec e1 inf_op e2
+																	| _ -> "Unimplemented"
+														| _ -> "Uninplemented"
+(* The functions to execute a variable declaration *)
 
 (* TODO : add all the type of exp *)
-let get_value_of_exp_desc exp exp_type=
-	match exp with
-		| Op(e1, inf_op, e2) -> 
-				(execute_op e1 inf_op e2 exp_type)
+let rec get_value_of_exp exp=
+	let exp_desc = exp.edesc in
+	let exp_type = exp.etype in
+	match exp_desc with
+		| Op(e1, inf_op, e2) ->
+				(execute_op (get_value_of_exp e1) inf_op (get_value_of_exp e2) exp_type)
 		| Val x -> match x with
 			| Int n -> n
 			| _ -> print_endline "not implemented"; "0"
 		| _ -> print_endline "not implemented"; "0"
 
-
-
-(* The functions to execute a variable declaration *)
-
-
 let execute_vardecl_aux one_vd mem= match one_vd with
-	| (type_ast, name, Some(exp_value)) -> 
+	| (type_ast, name, Some(exp_value)) ->
 		let variable_type = get_string_type_from_typet type_ast in
 		let expdesc_of_exp = exp_value.edesc in
 		let given_type = exp_value.etype in
-		let var_value = get_value_of_exp_desc expdesc_of_exp given_type in
+		let var_value = get_value_of_exp exp_value  in
 		(match given_type with
 				| None -> print_endline "lollololol"
 				| Some(type_found) -> print_endline ("LOOK HERE"^(Type.stringOf type_found)));
@@ -135,7 +134,7 @@ let execute_vardecl_aux one_vd mem= match one_vd with
 
 let rec execute_vardecl vd_list mem = match vd_list with
 	| [] -> print_endline "vardecl finished or empty"; mem
-	| h::q -> 
+	| h::q ->
 		let new_mem = execute_vardecl_aux h mem in
 				(execute_vardecl q new_mem )
 
@@ -145,17 +144,17 @@ let rec execute_vardecl vd_list mem = match vd_list with
 
 let execute_statement statement mem=
 	match statement with
-		| VarDecl dl -> execute_vardecl dl mem 
-		| _ -> print_endline "not implemented"; mem 
+		| VarDecl dl -> execute_vardecl dl mem
+		| _ -> print_endline "not implemented"; mem
 
 
 (* Prends en argument la liste des statements de la methode en cours d execution, ainsi que la memoire qui a ete initialisee *)
-let rec execute_statements  statement_list mem = 
+let rec execute_statements  statement_list mem =
 	match statement_list with
 		| [] -> print_endline "No more statement to execute \n";
 			print_endline "Here is the printing of the memory :";
 			mem
-		| h::t -> print_endline "Starting executing statements"; 
+		| h::t -> print_endline "Starting executing statements";
 				let new_mem = execute_statement h mem in
 				execute_statements t new_mem
 
@@ -163,16 +162,16 @@ let rec execute_statements  statement_list mem =
 let start_point astmethod_main =
 	let mem = Env.initial() in
 	let statement_list_main = astmethod_main.AST.mbody in
-		execute_statements statement_list_main mem 
-	
+		execute_statements statement_list_main mem
+
 
 (* Fonction pour chercher la methode main dans method table *)
 let find_main env =
-  	Env.iter (fun (key,value) ->  match (split_main key) with 
+  	Env.iter (fun (key,value) ->  match (split_main key) with
 								|  "main" -> let res =start_point value in
 									print_memory res;
 									()
-								| _ -> () 
-	) env 
+								| _ -> ()
+	) env
 
 (* end *)
--- Tests/TestsUnitairesExec.java
+++ Tests/TestsUnitairesExec.java
@@ -1,9 +1,9 @@
 public class A {
     int attr;
-    public static void main() { 
+    public static void main() {
         int x = 1;
         int d = 4;
-        int z = 1 + 2;
+        int z = 3 * (4 / 4);
     }
 
     public void m2() {
